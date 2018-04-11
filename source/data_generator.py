3baaf24f38153fe43781c657fd4faefea3a3e2f2
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-04-11 19:29:41 +0300

    Move functions to correct places

--- source/data_generator.py
+++ source/data_generator.py
@@ -1,4 +1,6 @@
 import pickle
+import glob
+import os
 import numpy as np
 
 from heston import heston_dynamic_milstein_scheme
@@ -25,3 +27,25 @@ def generate_data(parameters, number_of_samples=10, dt=0, T=0):
                         r, v, _ = generate_sample(kappa, theta, xi, rho, dt, T)
                         filename = f"k{kappa}_t{theta}_xi{xi}_rho{rho}__{idx}"
                         store_sample(r, v, filename)
+
+def move_file_to_folder(old_path, new_path):
+    os.rename(old_path, new_path)
+
+def split_data(train_perc, val_perc, test_perc):
+    assert train_perc + val_perc + test_perc == 1, "Whole dataset not used"
+    files = [os.path.basename(path) for path in glob.glob("data/*.p")]
+
+    N = len(files)
+    train_size = int(N * train_perc)
+    val_size = int(N * val_perc)
+
+    np.random.shuffle(files)
+    training_set, validation_set, test_set = files[:train_size], files[train_size:(train_size+val_size)], files[(train_size+val_size):]
+    for file in training_set:
+        move_file_to_folder(f"data/{file}", f"data/training/{file}")
+    for file in validation_set:
+        move_file_to_folder(f"data/{file}", f"data/validation/{file}")
+    for file in test_set:
+        move_file_to_folder(f"data/{file}", f"data/test/{file}")
+
+    return "data/training/*.p", "data/validation/*.p", "data/test/*.p"
