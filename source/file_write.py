cee036ec64c42f0293d219f07a8245981e01a454
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-04-13 13:33:48 +0300

    store label

--- source/file_write.py
+++ source/file_write.py
@@ -5,12 +5,15 @@ class FileWriter(object):
         self.path = f"data/{dataset_name}.h5"
         self.file = h5py.File(self.path, 'w')
         self.row_count = 0
-        max_shape = (None, H, W)
-        self.dset = self.file.create_dataset('data', shape=(0, H, W), maxshape=max_shape)
+        self.dset = self.file.create_dataset('data', shape=(0, H, W), maxshape=(None, H, W))
+        self.dset_label = self.file.create_dataset('label', shape=(0,), maxshape=(None,), dtype='i8')
 
-    def write_chunk(self, chunk):
+    def write_chunk(self, chunk, labels):
         self.dset.resize(self.row_count + chunk.shape[0], axis=0)
         self.dset[self.row_count:] = chunk
+
+        self.dset_label.resize(self.row_count + chunk.shape[0], axis=0)
+        self.dset_label[self.row_count:] = labels
         self.row_count += chunk.shape[0]
 
     def close_file(self):
@@ -25,11 +28,16 @@ if __name__ == "__main__":
     fw = FileWriter("test_db", 2, 3)
 
     for i in range(10):
-        fw.write_chunk(np.array([data]) * i)
+        fw.write_chunk(np.array([data]) * i, i)
 
     assert int(fw.dset[9][0][0]) == 9
     assert int(fw.dset[8][0][0]) == 8
     assert int(fw.dset[9][0][2]) == 45
+    assert fw.dset_label[9] == 9
+    assert fw.dset_label[8] == 8
+    assert fw.dset_label[8] != 9
+
+    import ipdb; ipdb.set_trace()
 
     fw.close_file()
     os.remove(fw.path)
