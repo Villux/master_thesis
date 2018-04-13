bf38ae0465d8f107ad6a059d685b132ae4e58be4
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-04-13 16:25:25 +0300

    dg rewrite

--- source/data_generator.py
+++ source/data_generator.py
@@ -5,59 +5,81 @@ import numpy as np
 from multiprocessing import Pool
 
 from heston import heston_dynamic_milstein_scheme
+from label_mapper import LabelMapper
+from file_write import FileWriter
 
-class SimulationEnginer(object):
-    def __init__(self, kappa, theta, xi, rho, dt, T):
-        self.kappa = kappa
-        self.theta = theta
-        self.xi = xi
-        self.rho = rho
+def generate_parameter_tuples(kappas, thetas, xis, rhos):
+    return tuple([(kappa, theta, xi, rho) for kappa in kappas
+            for theta in thetas
+                for xi in xis
+                    for rho in rhos])
+
+
+class DataGenerator(object):
+    def __init__(self, kappas, thetas, xis, rhos, dt, T, N, chunk_size=12):
         self.dt = dt
         self.T = T
-    def __call__(self, idx):
-        r, v, _ = generate_sample(self.kappa, self.theta, self.xi, self.rho, self.dt, self.T)
-        filename = f"k{self.kappa}_t{self.theta}_xi{self.xi}_rho{self.rho}__{idx}"
+        self.time_series_length = int(T/dt)
+        self.N = N
+        self.lm = LabelMapper(kappas, thetas, xis, rhos)
+        self.chunk_size = chunk_size
+        self.param_tuples = generate_parameter_tuples(kappas, thetas, xis, rhos)
 
-        return r, v, filename
+    def generate_data(self):
+        pool = Pool(os.cpu_count())
+        fw_training = FileWriter("training", 2, self.time_series_length)
+        fw_validation = FileWriter("validation", 2, self.time_series_length)
+        fw_test = FileWriter("test", 2, self.time_series_length)
 
-def generate_sample(kappa, theta, xi, rho, dt, T):
-    parameters = {
-        'kappa': kappa,
-        'theta': theta,
-        'xi': xi,
-        'rho': rho
-    }
-    return heston_dynamic_milstein_scheme(parameters=parameters, T=T, dt=dt)
+        for _ in range(int(self.N/self.chunk_size)):
+            data_tuple = pool.map(self.generate_sample, self.param_tuples * self.chunk_size)
 
-def generate_data(parameters, number_of_samples=10, dt=0, T=0):
-    pool = Pool(os.cpu_count())
+            X = np.empty((np.shape(data_tuple)[0], np.shape(data_tuple[0][0])[0], np.shape(data_tuple[0][0])[1]))
+            y = np.empty((np.shape(data_tuple)[0], 1), dtype=int)
 
-    for _, kappa in enumerate(parameters['kappa']):
-        for _, theta in enumerate(parameters['theta']):
-            for _, rho in enumerate(parameters['rho']):
-                for _, xi in enumerate(parameters['xi']):
-                    engine = SimulationEnginer(kappa, theta, xi, rho, dt, T)
-                    data = pool.map(engine, range(number_of_samples))
-    pool.close()
+            for idx, (data, label) in enumerate(data_tuple):
+                X[idx] = data
+                y[idx] = label
 
-def move_file_to_folder(old_path, new_path):
-    os.rename(old_path, new_path)
+            perm = np.random.permutation(len(y))
+            X = X[perm]
+            y = y[perm]
 
-def split_data(train_perc, val_perc, test_perc):
-    assert train_perc + val_perc + test_perc == 1, "Whole dataset not used"
-    files = [os.path.basename(path) for path in glob.glob("data/*.p")]
+            training_size = int(len(y) * 0.5)
+            validation_size = int(len(y) * 0.25)
 
-    N = len(files)
-    train_size = int(N * train_perc)
-    val_size = int(N * val_perc)
+            fw_training.write_chunk(X[:training_size], y[:training_size])
+            fw_validation.write_chunk(X[training_size:training_size + validation_size], y[training_size:training_size + validation_size])
+            fw_test.write_chunk(X[training_size+validation_size:], y[training_size+validation_size:])
 
-    np.random.shuffle(files)
-    training_set, validation_set, test_set = files[:train_size], files[train_size:(train_size+val_size)], files[(train_size+val_size):]
-    for file in training_set:
-        move_file_to_folder(f"data/{file}", f"data/training/{file}")
-    for file in validation_set:
-        move_file_to_folder(f"data/{file}", f"data/validation/{file}")
-    for file in test_set:
-        move_file_to_folder(f"data/{file}", f"data/test/{file}")
+        fw_training.close_file()
+        fw_validation.close_file()
+        fw_test.close_file()
 
-    return "data/training/*.p", "data/validation/*.p", "data/test/*.p"
+    def generate_sample(self, *args):
+        kappa, theta, xi, rho = args[0]
+        parameters = {
+            'kappa': kappa,
+            'theta': theta,
+            'xi': xi,
+            'rho': rho
+        }
+        r,v, _ = heston_dynamic_milstein_scheme(parameters=parameters, T=self.T, dt=self.dt)
+        label = self.lm.get_label(kappa, theta, xi, rho)
+        return ([r,v], label)
+
+if __name__ == "__main__":
+    # Test
+    import os
+    import numpy as np
+
+    kappa = [0.2, 2, 6]
+    theta = [0.1**2, 0.3**2, 0.5**2]
+    rho = [-0.1, -0.5, -0.9]
+    xi = [0.1, 0.3, 0.6]
+
+    dt = 1/2
+    T = 1
+
+    dg = DataGenerator(kappa, theta, xi, rho, dt, T, 100, chunk_size=8)
+    dg.generate_data()
