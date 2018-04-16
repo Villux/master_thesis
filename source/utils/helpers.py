68cbeaaec40e87928a2358ad322c3fec53e76c82
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-04-16 13:56:03 +0300

    simplify

--- source/utils/helpers.py
+++ source/utils/helpers.py
@@ -1,12 +1,9 @@
 from utils.batch_cache import BatchCache
-from utils.data_reader import DataReader
 from utils.file_reader_h5py import FileReaderH5py
 from utils.h5py_dataset import H5pyDataset
 
 def create_h5py_dataset_with_cache(dataset_name, batch_size):
     file_reader = FileReaderH5py(dataset_name)
-    data_reader = DataReader(file_reader)
-    cache = BatchCache(batch_size, data_reader.get_range)
-
-    return H5pyDataset(cache, data_reader)
+    cache = BatchCache(batch_size, file_reader.get_range)
+    return H5pyDataset(file_reader, cache=cache)
 
