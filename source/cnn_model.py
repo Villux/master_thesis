2e0ec8be0a971907cbd4c96b13dff17c03cb35d7
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-05-04 15:21:58 +0300

    remove model

--- source/cnn_model.py
+++ source/cnn_model.py
@@ -7,6 +7,7 @@ import torch.utils.data as Data
 
 from data_loader import load_data
 from utils.helpers import create_h5py_dataset_with_cache
+from models.fcn import FCN
 # Hyper Parameters
 EPOCH = 5
 BATCH_SIZE = 100
@@ -25,31 +26,11 @@ val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=BATCH_S
 test_dataset = create_h5py_dataset_with_cache("test", BATCH_SIZE)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv1d(n_channels, 18, kernel_size=10, stride=1),
-            nn.ReLU(),
-            nn.AvgPool1d(2))
-        self.layer2 = nn.Sequential(
-            nn.Conv1d(8, 4, kernel_size=5, stride=1),
-            nn.ReLU(),
-            nn.AvgPool1d(2))
-        self.fc = nn.Linear(58 * 4, n_classes)
 
-    def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
-
-cnn = CNN()
-
+fcn = FCN(n_channels)
 # Loss and Optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
+optimizer = torch.optim.Adam(fcn.parameters(), lr=LR)
 
 # Train the Model
 for epoch in range(EPOCH):
@@ -58,7 +39,7 @@ for epoch in range(EPOCH):
         labels = Variable(data['label']).type(torch.LongTensor)
         # Forward + Backward + Optimize
         optimizer.zero_grad()
-        outputs = cnn(tseries)
+        outputs = fcn(tseries)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
@@ -68,13 +49,13 @@ for epoch in range(EPOCH):
                    %(epoch+1, EPOCH, i+1, len(train_dataset)//BATCH_SIZE, loss.data[0]))
 
 # Test the Model
-cnn.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
+fcn.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
 correct = 0
 total = 0
 for i, data in enumerate(val_loader):
     tseries = Variable(data['data']).float()
     labels = Variable(data['label']).type(torch.LongTensor)
-    outputs = cnn(tseries)
+    outputs = fcn(tseries)
     _, predicted = torch.max(outputs.data, 1)
     total += labels.size(0)
     correct += (predicted == labels.data).sum()
@@ -86,7 +67,7 @@ total = 0
 for i, data in enumerate(test_loader):
     tseries = Variable(data['data']).float()
     labels = Variable(data['label']).type(torch.LongTensor)
-    outputs = cnn(tseries)
+    outputs = fcn(tseries)
     _, predicted = torch.max(outputs.data, 1)
     total += labels.size(0)
     correct += (predicted == labels.data).sum()
